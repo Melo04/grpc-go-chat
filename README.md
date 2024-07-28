3bd7bb4941fafe08825f0c78f1dd8bb345bc1501
Melo04 <kohhmelody@gmail.com>
2024-07-28 13:02:29 +0800

    server initialization

---
+++ README.md
@@ -0,0 +1,4 @@
+## gRPC Chat Server Golang
+This is a real-time chat server built using gRPC and Golang. It serves as a fundamental project for me to learn gRPC, concurrency and channels.
+
+## 
--- server/server.go
+++ server/server.go
@@ -1,12 +1,69 @@
 package server
 
-import "flag"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"sync"
 
-var (
-	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	certFile   = flag.String("cert_file", "", "The TLS cert file")
-	keyFile    = flag.String("key_file", "", "The TLS key file")
-	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
-	port       = flag.Int("port", 50051, "The server port")
+	pb "github.com/Melo04/grpc-chat/pb"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
 )
 
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedChatServerServer
+	mu       sync.Mutex
+	servers  map[string]*ChatServer
+	messages map[string][]*pb.Message
+}
+
+type ChatServer struct {
+	ID   string
+	Name string
+}
+
+func NewServer() *server {
+	return &server{
+		servers:  make(map[string]*ChatServer),
+		messages: make(map[string][]*pb.Message),
+	}
+}
+
+func (s *server) CreateChatServer(ctx context.Context, req *pb.CreateChatServerRequest) (*pb.CreateChatServerResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	//generate server id dynamically
+	serverID := uuid.New().String()
+
+	chatServer := &ChatServer{
+		ID:   serverID,
+		Name: req.GetServerName(),
+	}
+
+	s.servers[serverID] = chatServer
+
+	return &pb.CreateChatServerResponse{ServerId: serverID}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterChatServerServer(grpcServer, NewServer())
+
+	log.Println("Starting server on port", *port)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
