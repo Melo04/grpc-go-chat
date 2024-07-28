e3e2304b5ca0ae4f15e0766cb0585b53a0a6f47a
Melo04 <kohhmelody@gmail.com>
2024-07-28 14:50:48 +0800

    server implementation

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"sync"
@@ -11,6 +12,7 @@ import (
 	pb "github.com/Melo04/grpc-chat/pb"
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 var (
@@ -51,6 +53,101 @@ func (s *server) CreateChatServer(ctx context.Context, req *pb.CreateChatServerR
 	s.servers[serverID] = chatServer
 
 	return &pb.CreateChatServerResponse{ServerId: serverID}, nil
+}
+
+func (s *server) JoinChatServer(ctx context.Context, req *pb.JoinChatServerRequest) (*pb.JoinChatServerResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	welcomeMessage := req.GetUsername() + " just slid into the server " + req.GetServerId()
+	return &pb.JoinChatServerResponse{WelcomeMessage: welcomeMessage}, nil
+}
+
+func (s *server) LeaveChatServer(ctx context.Context, req *pb.LeaveChatServerRequest) (*pb.LeaveChatServerResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	goodbyeMessage := req.GetUsername() + " just left the server"
+	return &pb.LeaveChatServerResponse{GoodbyeMessage: goodbyeMessage}, nil
+}
+
+func (s *server) ListMessages(req *pb.ListMessagesRequest, stream pb.ChatServer_ListMessagesServer) error {
+	s.mu.Lock()
+	messages, ok := s.messages[req.GetChannelId()]
+	s.mu.Unlock()
+
+	if !ok {
+		return fmt.Errorf("channel not found")
+	}
+
+	for _, message := range messages {
+		if err := stream.Send(message); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *server) SendMessages(stream pb.ChatServer_SendMessagesServer) error {
+	var messageCount int32
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.SendMessagesResponse{
+				MessageCount: messageCount,
+			})
+		}
+
+		if err != nil {
+			return err
+		}
+
+		s.mu.Lock()
+		s.messages[req.GetChannelId()] = append(s.messages[req.GetChannelId()], &pb.Message{
+			Username: req.GetUsername(),
+			Text: req.GetText(),
+			Timestamp: timestamppb.Now(),
+		})
+		s.mu.Unlock()
+
+		log.Printf("Message received from %s: %s", req.GetUsername(), req.GetText())
+		messageCount++
+	}
+}
+
+func (s *server) Chat(stream pb.ChatServer_ChatServer) error {
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Message received from %s: %s", in.GetUsername(), in.GetText())
+
+		s.mu.Lock()
+		// ChatMessage doesnt have a channel id, so we use the username as the channel id
+		s.messages[in.GetChannelId()] = append(s.messages[in.GetChannelId()], &pb.Message{
+			Username:  in.GetUsername(),
+			Text:      in.GetText(),
+			Timestamp: timestamppb.Now(),
+		})
+		s.mu.Unlock()
+
+		// Echo the message back to the client
+		if err := stream.Send(&pb.ChatMessage{
+			ServerId: in.GetServerId(),
+			ChannelId: in.GetChannelId(),
+			Username:  in.GetUsername(),
+			Text:      in.GetText(),
+			Timestamp: timestamppb.Now(),
+		}); err != nil {
+			return err
+		}
+	}
 }
 
 func main() {
